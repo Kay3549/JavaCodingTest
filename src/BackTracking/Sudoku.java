71b043e20c7fc528be96b27ee0bdbef4ab38cd22
wjdfu <thebestglorious@naver.com>
2022-03-28 20:05:58 +0900

    create project

--- src/BackTracking/Sudoku.java
+++ src/BackTracking/Sudoku.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public class Sudoku {
 
 	public static void main(String[] args) {
-		
+		  
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
