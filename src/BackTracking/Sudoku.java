6da410e48290d9ec664446dc26d5ca742260ebf8
wjdfu <thebestglorious@naver.com>
2022-03-28 20:03:24 +0900

    create project

--- src/BackTracking/Sudoku.java
+++ src/BackTracking/Sudoku.java
@@ -1,10 +1,20 @@
 package BackTracking;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 public class Sudoku {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			
+		} catch (Exception e) {
+		}
+		
 	}
 
 }
