fb6f8ad0d27f3d98b03d236064e9b997a5e9bbe9
Kang-WonHyung <kay3549@infognc.co.kr>
2025-02-28 17:00:54 +0900

    .

---
+++ src/programmers/모의고사1회/number1.java
@@ -0,0 +1,10 @@
+package programmers.모의고사1회;
+
+//모의고사 1회 1번문제 '외톨이 알파벳'
+public class number1 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, asdfadf!");
+    }
+
+}
