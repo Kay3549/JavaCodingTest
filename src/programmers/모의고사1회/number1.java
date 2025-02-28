297c0786eca9a1ea3b3cdf0d18779dabac582996
Kang-WonHyung <kay3549@infognc.co.kr>
2025-02-28 17:44:58 +0900

    문제 풀이 중

--- src/programmers/모의고사1회/number1.java
+++ src/programmers/모의고사1회/number1.java
@@ -1,10 +1,54 @@
 package programmers.모의고사1회;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 //모의고사 1회 1번문제 '외톨이 알파벳'
 public class number1 {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, asdfadf!");  
-    }  
+        solution("dlkfjasdfj");
+
+    }
+
+    public static String solution(String input_string) {
+
+        int len = input_string.length();
+        Map<Character, List<Integer>> charlists = new HashMap<Character, List<Integer>>();
+
+        for(int i =0;i<len; i++){
+            Character key = input_string.charAt(i);
+            if (!charlists.containsKey(key)) {
+                charlists.put(key, new ArrayList<>());
+                charlists.get(key).add(i);
+            }else{
+                charlists.get(key).add(i);
+            }
+        }
+
+
+        for (Map.Entry<Character, List<Integer>> entry : charlists.entrySet()) {
+
+            List<Integer> li = new ArrayList<>();
+            li = entry.getValue();
+            int li_size = entry.getValue().size();
+
+            if (li_size >= 2) {
+                
+            }
+        }
+
+
+
+        String answer = input_string;
+        System.out.println(answer);
+        return answer;
+    }
+
+
+    
+    
 
 }
