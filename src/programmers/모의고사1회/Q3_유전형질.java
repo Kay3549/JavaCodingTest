e5ce39086b7defc294b6a3bcfb0f3b8cc7baebf5
Kang-WonHyung <kay3549@infognc.co.kr>
2025-03-10 17:48:29 +0900

    풀이중

--- src/programmers/모의고사1회/Q3_유전형질.java
+++ src/programmers/모의고사1회/Q3_유전형질.java
@@ -7,15 +7,25 @@ public class Q3_유전형질 {
 
         int [][] arr1 = {{3,5}};
         int [][] arr2 = {{3,8},{2,2}};
-        solution(arr1);
+        solution(arr2);
     }
     
 
     public static String[] solution(int[][] queries) {  
+
+        int rep = queries.length; // 전체 반복 횟수. 2번
+        for(int i = 0; i<rep; i++){
+            int generation = queries[i][0]; //3세대, 2세대
+            asdf(generation);
+
+        }
+
+
+        
         String[] answer = {};
         return answer;
     }
-
+    
 
     public static long asdf (int n){
 
