3e9d437db77b281fc32b2477d79804f9048194f1
Kang-WonHyung <kay3549@infognc.co.kr>
2025-03-24 16:11:34 +0900

    completed Q3

--- src/programmers/모의고사1회/Q3_유전형질.java
+++ src/programmers/모의고사1회/Q3_유전형질.java
@@ -1,37 +1,38 @@
 package programmers.모의고사1회;
 
 public class Q3_유전형질 {
+    public static void main(String[] args) {
+        int[][] arr2 = { { 3, 5 }, { 2, 2 } };
+        String[] result = solution(arr2);
 
-
-    public static void main(String[] args) {  
-
-        int [][] arr1 = {{3,5}};
-        int [][] arr2 = {{3,8},{2,2}};
-        solution(arr2);
+        for (String s : result) {
+            System.out.println(s); // 결과 출력
+        }
     }
-    
 
     public static String[] solution(int[][] queries) {  
+        int rep = queries.length;  
+        String[] answer = new String[rep]; // 배열 초기화
 
-        int rep = queries.length; // 전체 반복 횟수. 2번
-        for(int i = 0; i<rep; i++){
-            int generation = queries[i][0]; //3세대, 2세대
-            asdf(generation);
+        for (int i = 0; i < rep; i++) {  
+            int generation = queries[i][0];  
+            int order = queries[i][1] - 1; // index 0번 부터 시작하는 걸로 변경
 
+            answer[i] = find(generation, order); // 재귀
         }
 
-
-        
-        String[] answer = {};
-        return answer;
-    }
-    
-
-    public static long asdf (int n){
-
-        long k = (long)Math.pow(4, n - 1);
-        System.out.println(k);
-        return k;
+        return answer;  
     }
 
+    private static String find(int n, int p) {
+        if (n == 1) return "Rr"; // 기본 케이스, 루트
+
+        int slice = (int) Math.pow(4, n - 2);  
+        int group = p / slice; //어느 그룹에 속하는지 결정
+
+        if (group == 0) return "RR";  
+        if (group == 3) return "rr";  
+
+        return find(n - 1, p % slice); // 재귀
+    }
 }
